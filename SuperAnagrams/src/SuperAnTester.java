924e3753eeee0767d105f8ed7968375b09af5b40
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-10-26 09:14:54 -0700

    feat: Add Comments and finish code

--- SuperAnagrams/src/SuperAnTester.java
+++ SuperAnagrams/src/SuperAnTester.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
+/**
+ * Given two phrases, are they super anagrams regardless of capitalization and
+ * punctuation?
+ */
 public class SuperAnTester {
-	public static void main(String[] args, boolean gram) {
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a phrase:" + "\n" + "> ");
 		String strOne = scan.nextLine();
+		System.out.print("Enter another phrase:" + "\n" + "> ");
 		String strTwo = scan.nextLine();
-		System.out.println("Enter a phrase:" + strOne);
-		System.out.println("Enter a phrase:" + strTwo);
 		SuperAnagram ana = new SuperAnagram(strOne, strTwo);
 		if (ana.isSuperAnagram() == true) {
 			System.out.println("true");
-		}
-		else (ana.isSuperAnagram() == false) {
+		} else {
 			System.out.println("false");
 		}
 	}
